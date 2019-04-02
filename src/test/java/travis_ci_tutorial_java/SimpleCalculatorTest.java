87925d934638f495cb87ba420b52b351912c4e1a
Jonathan Zgorski <jonathanzgorski@gmail.com>
2019-04-02 17:17:01 -0400

    Add test case for divide by 0

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -29,4 +29,10 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(15,3), 5);
 	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDivideByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.divide(1, 0);
+	}
 }
